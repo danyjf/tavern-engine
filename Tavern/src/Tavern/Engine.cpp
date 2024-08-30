8ec7d20569041767424ce66973de617e49ad711d
Daniel Francisco <deny.jo@hotmail.com>
2024-08-30 12:31:00 +0100

    Render a square using element buffer object

--- Tavern/src/Tavern/Engine.cpp
+++ Tavern/src/Tavern/Engine.cpp
@@ -98,17 +98,28 @@ namespace Tavern
 
 		// Create vertex buffer object
 		float vertices[] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+			 0.5f,  0.5f, 0.0f, // top right
+			 0.5f, -0.5f, 0.0f,	// bottom right
+			-0.5f, -0.5f, 0.0f,	// bottom left
+			-0.5f,  0.5f, 0.0f,	// top left
+		};
+		unsigned int indices[] = {
+			0, 1, 3,
+			1, 2, 3
 		};
 
-		unsigned int VAO, VBO;
+		unsigned int VAO, VBO, EBO;
 		glGenVertexArrays(1, &VAO);
 		glGenBuffers(1, &VBO);
+		glGenBuffers(1, &EBO);
+
 		glBindVertexArray(VAO);
+
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 		// Link vertex attributes
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -133,12 +144,12 @@ namespace Tavern
 			// Render ----------------------------------
 			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT);
-	
 
 			// Draw the triangle
 			glUseProgram(shaderProgram);
 			glBindVertexArray(VAO);
-			glDrawArrays(GL_TRIANGLES, 0, 3);
+			//glDrawArrays(GL_TRIANGLES, 0, 3);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 			glfwPollEvents();
 			glfwSwapBuffers(m_Window->GetGLFWWindow());
