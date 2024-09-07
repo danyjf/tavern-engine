c898357f479b73dc09b7e61ef1e0175f23bd1529
Daniel Francisco <76452190+danyjf@users.noreply.github.com>
2024-09-07 23:01:33 +0100

    Parameterize project generation batch script

--- GenerateProjects.bat
+++ GenerateProjects.bat
@@ -1,3 +1,36 @@
-CALL cmake -S . -B ./Build/ -G "Visual Studio 17 2022" -A x64
-PAUSE
+@ECHO OFF
+
+IF "%1"=="" (
+    ECHO One of the following generators is required as the first argument:
+    ECHO.   vs2022
+    ECHO.   ninja
+    ECHO Ex. GenerateProjects ^<generator^>
+    PAUSE
+    EXIT /b
+)
+
+IF "%1"=="vs2022" (
+    CALL cmake -S . -B ./Build/ -G "Visual Studio 17 2022" -A x64
+    EXIT /b
+)
+
+IF "%1"=="ninja" (
+    IF "%2"=="" (
+        ECHO When using this generator the build type must be passed as the second argument:
+        ECHO.   debug
+        ECHO.   release
+        ECHO Ex. GenerateProjects ^<generator^> ^<build_type^>
+        PAUSE
+        EXIT /b
+    )
+
+    IF "%2"=="debug" (
+        CALL cmake -S . -B ./Build/ -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang.exe -DCMAKE_C_COMPILER=clang.exe
+        EXIT /b
+    )
+    IF "%2"=="release" (
+        CALL cmake -S . -B ./Build/ -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=clang.exe -DCMAKE_C_COMPILER=clang.exe
+        EXIT /b
+    )
+)
 
--- GenerateProjects_Ninja.bat
+++
@@ -1,3 +0,0 @@
-CALL cmake -S . -B ./Build/ -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang.exe -DCMAKE_C_COMPILER=clang.exe
-PAUSE
-
