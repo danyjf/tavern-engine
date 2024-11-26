c5d397b93d784ee3b5661520521015a96a709b3b
Daniel Francisco <deny.jo@hotmail.com>
2024-11-26 20:52:19 +0000

    Add docking with a menu bar to the editor

--- Taverner/src/Taverner/Windows/EditorWindow.cpp
+++ Taverner/src/Taverner/Windows/EditorWindow.cpp
@@ -22,7 +22,34 @@ namespace Taverner
 		ImGui::NewFrame();
 
 		// Display windows here
+		ImGuiWindowFlags windowFlags = ImGuiWindowFlags_NoTitleBar
+			| ImGuiWindowFlags_NoDocking
+			| ImGuiWindowFlags_NoCollapse 
+			| ImGuiWindowFlags_NoResize 
+			| ImGuiWindowFlags_NoMove 
+			| ImGuiWindowFlags_NoBringToFrontOnFocus 
+			| ImGuiWindowFlags_NoBackground
+			| ImGuiWindowFlags_NoNavFocus;
+		ImGui::Begin("Project", nullptr, windowFlags);
+
+		ImGuiDockNodeFlags dockspaceFlags = ImGuiDockNodeFlags_PassthruCentralNode;
+		ImGui::DockSpaceOverViewport(0, ImGui::GetMainViewport(), dockspaceFlags);
+
+		if (ImGui::BeginMainMenuBar())
+		{
+			if (ImGui::BeginMenu("File"))
+			{
+				ImGui::MenuItem("New Project");
+				ImGui::MenuItem("Open Project");
+				ImGui::EndMenu();
+			}
+
+			ImGui::EndMainMenuBar();
+		}
+
 		ImGui::ShowDemoWindow();
+
+		ImGui::End();
 
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
